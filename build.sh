3987f66caab3a23d93d8aa2decb7ab554ae6d4af
huss2342 <a.hussein@wustl.edu>
2025-02-11 21:52:29 -0600

    tested the ROBOTICS

--- build.sh
+++ build.sh
@@ -3,6 +3,20 @@ mkdir -p build && cd build
 cmake ../
 make 
 
-$LLVM_DIR/bin/clang++ -emit-llvm -S ../cfi_icall.c -o cfi_icall.ll
-$LLVM_DIR/bin/opt -f -enable-new-pm=0 -load ./libHelloWorld.so -hello -S cfi_icall.ll -o transformed_cfi_call.ll
-$LLVM_DIR/bin/clang++ transformed_cfi_call.ll ../cfi_check_lib.cpp -o exe_cfi_icall
+# $LLVM_DIR/bin/clang++ -emit-llvm -S ../cfi_icall.c -o cfi_icall.ll
+# $LLVM_DIR/bin/opt -f -enable-new-pm=0 -load ./libHelloWorld.so -hello -S cfi_icall.ll -o transformed_cfi_call.ll
+# $LLVM_DIR/bin/clang++ transformed_cfi_call.ll ../cfi_check_lib.cpp -o exe_cfi_icall
+
+
+BUILDING="/home/cse569s/nevin/CppRobotics/src/rrt.cpp"
+INCLUDES="/home/cse569s/nevin/CppRobotics/include"
+PATH_TO_BIN="/home/cse569s/nevin/CppRobotics/bin"
+OPENCV_INCLUDE_DIRS=$(pkg-config --cflags opencv4)
+OPENCV_LIBS=$(pkg-config --libs opencv4)
+
+$LLVM_DIR/bin/clang++ -emit-llvm -S $BUILDING -o $PATH_TO_BIN/rrt.ll -I$INCLUDES $OPENCV_INCLUDE_DIRS
+$LLVM_DIR/bin/opt -f -enable-new-pm=0 -load ./libHelloWorld.so -hello -S $PATH_TO_BIN/rrt.ll -o $PATH_TO_BIN/transformed_rrt.ll
+$LLVM_DIR/bin/clang++ $PATH_TO_BIN/transformed_rrt.ll ../cfi_check_lib.cpp -o $PATH_TO_BIN/exe_rrt $OPENCV_LIBS
+
+
+
--- cfi_check_lib.cpp
+++ cfi_check_lib.cpp
@@ -2,12 +2,13 @@
 #include <unordered_map>
 #include <unordered_set>
 
+// for cfi_icall
 // Input 0: BB-1 → BB0 → BB2 → BB4 → BB3
 // Input 1: BB-1 → BB0 → BB2 → BB5 → BB3
 // Input 2: BB-1 → BB0 → BB2 → BB6 (runtime error)
 // Input 3: BB-1 → BB0 → BB2 → BB3 (segmentation fault)
 
-#define DEBUG 0
+#define DEBUG 1
 
 #if DEBUG
 #include <iostream>
@@ -17,9 +18,11 @@ std::unordered_map<int, std::unordered_set<int>> ValidTransitions;
 int currentBBID = -1;
 
 extern "C" void initCFG() {
-  ValidTransitions[0].insert(2);
-  ValidTransitions[2].insert(4);
-  ValidTransitions[2].insert(5);
+//   ValidTransitions[0].insert(2);
+//   ValidTransitions[2].insert(4);
+//   ValidTransitions[2].insert(5);
+    ValidTransitions[-1].insert(323);
+    ValidTransitions[323].insert(324);
 
 #if DEBUG
   std::cerr << "Valid transitions map:\n";
